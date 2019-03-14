7bbae8222c76c143e2278569b031d32bdcaa47a0
joeyheijm <42804866+joeyheijm@users.noreply.github.com>
2019-03-14 13:14:32 +0100

    Add files via upload
    beginnen met programmeren

---
+++ opdracht2/index.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: joey
+ * Date: 13/03/2019
+ * Time: 18:30
+ */
+
+//$aantal = 14;
+//$deze_jongen = "deze jongen is een zieke man van";
+//
+//echo $deze_jongen.$aantal."!!!";
+
+$a = "ik heb niet gefaald.";
+$b = "ik heb alleen 10000 manieren";
+$c = " gevonden die niet werken.";
+
+echo $a.$b.$c." <br> -thomas edison";
+
+
+
+$e = " Mensen die";
+$f = " geen fouten maken";
+$g = " werken niet.";
+
+echo $e.$f.$g."<br> -Albert Plesman (KLM)";
+
+$h = " Try and fail,";
+$i = " but never fail";
+$j = " to try";
+
+echo $h.$i.$j."<br> -Tyra Banks";
+
+$k = " De cursus omgaan met teleurstelling";
+$l = " kan vanavond helaas";
+$m = " niet door gaan.";
+
+echo $k.$l.$m."<br> -Herman Finkers";
